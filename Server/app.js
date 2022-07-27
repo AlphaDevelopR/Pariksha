ab46fb3e5960645dae9b4b3bbcb60f217e54d98a
AnkitRawat9442 <gehurecruitankitrawat@gmail.com>
2022-07-27 21:09:13 +0530

    Ankit added files that save the decrypted password in the mongoDB using bcrypt

--- Server/app.js
+++ Server/app.js
@@ -4,7 +4,8 @@ const PORT = process.env.PORT || 2000;
 const mongoose =  require("mongoose");
 const User = require("./models/User");
 const cors = require("cors");
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
 mongoose.connect("mongodb+srv://Ankit9442:Ankitrawat9442@cluster0.tqs5b.mongodb.net/?retryWrites=true&w=majority" , {
     useNewUrlParser : true
 }).then(console.log("mongoose connected ....")).catch((err)=>console.log(err));
@@ -12,24 +13,29 @@ mongoose.connect("mongodb+srv://Ankit9442:Ankitrawat9442@cluster0.tqs5b.mongodb.
 
 
 const newUser = new User( {
-  StudentID : "12345",
-  password : "Ankit@123" 
+  StudentID : "ankitrawat20052001@gmail.com",
+  password : "Ankitrawat9442" 
 });
 bcrypt.genSalt(saltRounds, function (err, salt) {
   // generating salt for the user password
   bcrypt.hash(newUser.password, salt, function (err, hash) {
       // returns hash
        newUser.password = hash;
-console.log(newUser.password);
-// newUser.save().then((u)=>{
-//  console.log("saved..");
-//  res.send("heloo");
-//  }).catch((e)=>{
-//   console.log(e);
-//  })
+      
+      //  newUser.save().then((u)=>{
+      //   console.log("saved..");
+        
+      //   }).catch((e)=>{
+      //    console.log(e);
+      //   })
+  })});
+
+
 
   app.use(express.json());
   app.use(cors());
+
+
 
 app.get("/" , (req , res)=>{
   res.send("helloo");
@@ -38,11 +44,11 @@ app.get("/" , (req , res)=>{
 app.post("/insert" ,  (req , res) =>{
 
  //const user = req.body;
-
- // console.log(typeof user);
-  const stdid = req.body.studentID;
-  const password = req.body.password;
  
+ // console.log(typeof user);
+  const stdid = req.body.studentID; 
+  const password = req.body.password;
+  
  
   // User.find({$where : {StudentID : stdid}} , (err , result )=>{
   //   if(err)
@@ -52,19 +58,29 @@ app.post("/insert" ,  (req , res) =>{
   //     console.log("login successfully ");
   // });
 
+
+  
   User.findOne({StudentID : stdid }).then((user)=>{
     if(user)
-    {
-      // console.log(user);
-      if(user.password === password)
-      {
-        console.log("login successfull");
-      }
+    { console.log(user);
+       // Match password
+       bcrypt.compare(password, user.password, (err, isMatch) => {
+        if (err) throw err;
+        if (isMatch) {
+          console.log("login successfully ");
+   
+        } else {
+         console.log("password not matched ");
+        }
+      });
+    }
       else
       console.log("you are not registered ....")
-    }
+    
     
   })
+  
+
 
 });
 
--- Server/models/User.js
+++ Server/models/User.js
@@ -1,6 +1,26 @@
 const mongoose =  require("mongoose");
 
 const UserSchema = new mongoose.Schema ({
+    // FName : {
+    //     type : String ,
+    //     required :true
+    // },
+    // LName : {
+    //     type : String ,
+    //     required :true
+    // },
+    // Gender : {
+    //     type : String ,
+    //     required :true
+    // },
+    // Email : {
+    //     type : String ,
+    //     required :true
+    // },
+    // phoneNumber : {
+    //     type : String ,
+    //     required :true
+    // },
     StudentID : {
           type : String ,
           required : true
@@ -10,7 +30,6 @@ const UserSchema = new mongoose.Schema ({
           required : true
       } 
 
-    
 });
 
 const User = mongoose.model("User" , UserSchema);
