761a05ab159b1869fa57aa58d153ac23cb2524b2
Hrishabhjoshi <hrishabhjoshi8@gmail.com>
2022-08-01 10:42:14 +0530

    fronted added

---
+++ src/components/pages/Home.js
@@ -0,0 +1,16 @@
+import { Grid } from "@mui/material";
+
+const Home = () => {
+  return <>
+    <Grid container justifyContent='center'>
+      <Grid item sm={10}>
+        <h1>Home Page</h1>
+        <hr />
+        <p>Hello Students</p>
+        <img src="C:\Users\hrish\Desktop\loginregreactjsmui1-master\src\images\homepage.png"/>
+      </Grid>
+    </Grid>
+  </>;
+};
+
+export default Home;
